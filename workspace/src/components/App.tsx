13b68620147b01ffa49715d5497569b1f0aa16b8
Nils Hartmann <nils@nilshartmann.net>
2026-04-13 11:09:04 +0200

    02: Props

--- workspace/src/components/App.tsx
+++ workspace/src/components/App.tsx
@@ -3,7 +3,12 @@ import PlantCard from "./PlantCard.tsx";
 export default function App() {
   return (
     <div className={"AppContainer"}>
-      <PlantCard />
+      <PlantCard
+        name={"Tulpe"}
+        location={"Küche"}
+        wateringInterval={7}
+        lastWatered="2026-04-13"
+      />
     </div>
   );
 }
--- workspace/src/components/PlantCard.tsx
+++ workspace/src/components/PlantCard.tsx
@@ -1,16 +1,61 @@
-export default function PlantCard() {
 
+//
+//
+//
+//
+//
+
+/*
+{
+name: "Tulpe"
+location: "Schlafzimmer"
+wateringInterval: 7
+lastWatered: "2026-04-13"
+}
+ */
+
+
+// interface IPlantCardProps {
+//   name: string;
+//   location: string;
+//   wateringInterval: number;
+//   lastWatered: string;
+// }
+
+
+type PlantCardProps = {
+  name: string;
+  location: string;
+  wateringInterval: number;
+  lastWatered?: string;
+};
+
+/// export default function PlantCard( props : PlantCardProps) {
+// Destructuring Operator { ... }
+export default function PlantCard( { name, location, lastWatered, wateringInterval }: PlantCardProps) {
   // JSX
+  // Properties (XML/HTML Attribute)
+  //   class -> className
+  //   html-for -> htmlFor
+
+  const wateringInfo = wateringInterval === 1
+    ? "Jeden Tag gießen" : `Alle ${wateringInterval} Tage gießen`;
+
+  // Alternativ ohne Destructuring mit "props.:
+  // const wateringInfo =
+  //   wateringInterval === 1
+  //     ? "Jeden Tag gießen"
+  //     : `Alle ${props.wateringInterval} Tage gießen`;
 
   return (
     <div className={"PlantCard"}>
       <header>
-        <h2>Aloe Vera</h2>
-        <div>📍Schlafzimmer</div>
+        <h2>{name}</h2>
+        <div>📍{location}</div>
       </header>
       <section>
-        <div>Alle 5 Tage gießen</div>
-        <div>Zuletzt: 24.06.2025</div>
+        <div>{wateringInfo}</div>
+        {lastWatered ? <div>Zuletzt: {lastWatered}</div> : null }
       </section>
     </div>
   );
