4ab1581e507def6385171b940805f82d61ef8120
Nils Hartmann <nils@nilshartmann.net>
2026-04-13 10:19:08 +0200

    01: Komponente mit statischem JSX-Code

--- workspace/src/components/PlantCard.tsx
+++ workspace/src/components/PlantCard.tsx
@@ -2,7 +2,16 @@ export default function PlantCard() {
 
   // JSX
 
-  return <div>
-           <h1>Hallo Welt</h1>
-         </div>;
+  return (
+    <div className={"PlantCard"}>
+      <header>
+        <h2>Aloe Vera</h2>
+        <div>📍Schlafzimmer</div>
+      </header>
+      <section>
+        <div>Alle 5 Tage gießen</div>
+        <div>Zuletzt: 24.06.2025</div>
+      </section>
+    </div>
+  );
 }
